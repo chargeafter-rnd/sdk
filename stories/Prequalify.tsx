842bd9f13525d89394383016e5decfedf824ec85
IgorSokolovsky <135001347+IgorSokolovsky@users.noreply.github.com>
2023-07-23 14:17:22 +0300

    fix test

--- stories/Prequalify.tsx
+++ stories/Prequalify.tsx
@@ -1,55 +1,56 @@
 import React from 'react';
-import { EnvironmentType, prequalify } from "../src";
+import { prequalify } from '../src';
+import { EnvironmentType } from '../src/initialize';
 
 interface ButtonProps {
+  firstName?: string;
 
-    firstName?: string;
+  /**
+   * Public API key
+   */
+  apiKey: string;
 
-    /**
-     * Public API key
-     */
-    apiKey: string;
-
-    /**
-     * Environment to work with
-     */
-    env?: EnvironmentType;
+  /**
+   * Environment to work with
+   */
+  env?: EnvironmentType;
 }
 
 /**
  * Primary UI component for user interaction
  */
 export const Button = ({
-    firstName,
-    apiKey,
-    env = "production",
-    ...props
+  firstName,
+  apiKey,
+  env = 'production',
+  ...props
 }: ButtonProps) => {
-
-    return (
-        <button
-            type="button"
-            {...props}
-            onClick={async () => {
-                try {
-                    await prequalify({
-                        config: {
-                            env: {
-                                name: env,
-                                apiKey: apiKey,
-                            }
-                        },
-                        consumerDetails: {
-                            firstName
-                        },
-                        onModalOpen: () => {
-                            console.log("##############opened###########")
-                        }
-                    });
-                } catch (e) {
-                    console.error(e);
-                }
-            }}
-        >Launch ChargeAfter modal</button >
-    );
+  return (
+    <button
+      type="button"
+      {...props}
+      onClick={async () => {
+        try {
+          await prequalify({
+            config: {
+              env: {
+                name: env,
+                apiKey: apiKey,
+              },
+            },
+            consumerDetails: {
+              firstName,
+            },
+            onModalOpen: () => {
+              console.log('##############opened###########');
+            },
+          });
+        } catch (e) {
+          console.error(e);
+        }
+      }}
+    >
+      Launch ChargeAfter modal
+    </button>
+  );
 };
